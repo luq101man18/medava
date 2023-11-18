91d55958a3d276ead20121c9e9c3e3128d1fc4e9
Luqman Al Hasni <67410294+luq101man18@users.noreply.github.com>
2023-11-18 15:30:43 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,37 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
+public abstract class Medicine implements Shippable {
 
+    public Medicine(String medicineName){
+        mMedicineName = medicineName;
+        
+    }
+    private String mMedicineName;
+
+    // getter: allows certain entities to see a value that has a private access modifier
+    public String getMedicineName(){
+        return mMedicineName;
+    }
+
+    // these two methods declarations will be overrides in subclasses, using virtual methods feature on java
+    public double minimumTemperature(){
+        return 0.0;
+    }
+    public double maximumTemperature(){
+        return 100.0;
+    }
+
+
+    // this function to determine the acceptable range of temperatures for medicines 
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature()){
+            return true;
+        }
+        return false;
+    }
+
+
+    public abstract MedicineSchedule getSchedule();
+    
+    
 }
