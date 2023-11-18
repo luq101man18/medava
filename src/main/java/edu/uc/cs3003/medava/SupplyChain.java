f96157a0738557869dbd3e479e7e456e6760d0a1
Luqman Al Hasni <67410294+luq101man18@users.noreply.github.com>
2023-11-18 15:33:12 -0500

    Update SupplyChain.java

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,5 +2,16 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
+    
+    //System.out.println("Hello, Luqman! This is the WOPR speaking.");
+
+    // to run the simulation of the HospitalRunner class
+    // the run method in the HospitalRunner class is static
+    // therefore, no instantiation
+    HospitalRunner.run();
+
+
+
+
     }
 }
