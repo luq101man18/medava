5dea2acc6c054024e791e3842736e52154d3bb6d
Luqman Al Hasni <67410294+luq101man18@users.noreply.github.com>
2023-11-18 15:29:17 -0500

    Update HospitalRunner.java

--- src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
+++ src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
@@ -1,1 +1,18 @@
 package edu.uc.cs3003.medava;
+
+// this will represent the simulation of sending and recieving 
+// via a transporter ( while the transporter works as a meduim )
+
+public class HospitalRunner {
+    public static void run() {
+        Transporter priorityDispatch = new Transporter("Priority Dispatch", 40.0, 41.0);
+
+        Pharmacy cvs = new Pharmacy("CVS at 7500 Beechmont Avenue");
+
+        cvs.send(priorityDispatch);
+
+        Hospital uc = new Hospital("World Famous University of Cincinnati Children's Hospital");
+
+        uc.receive(priorityDispatch);
+    }
+}
